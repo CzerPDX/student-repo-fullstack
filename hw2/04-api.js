fe01e6a87a8f1e1f45c51ef986fee7192de44581
CzerPDX <czer@pdx.edu>
2022-10-20 02:42:18 -0700

    added solution for 04-api.js

--- hw2/04-api.js
+++ hw2/04-api.js
@@ -1,7 +1,32 @@
-/** Exercise 04 - API **/
+/** Exercise 04 - API
+ * Brooke Czerwinski
+ * Full-Stack Web Development
+ * HW 2
+ * 
+ * **/
+
 
 const url = 'https://restcountries.com/v3.1/all';
 
-// Add your code here
 
-getData(url);
+fetch(url)
+// Get promise
+.then((response) => {
+    return response.json();
+})
+// Get data
+.then((data) => {
+
+    let countryList = ``
+    // Generate list items to insert into the DOM
+    data.forEach(country => {
+        // Save population formatted for the united states
+        populationFormatted = country.population.toLocaleString("en-US");
+        countryList += `<li> ${country.name.common} - ${populationFormatted}`;
+    });
+
+    // Insert country list into page
+    document.getElementById(`results`).innerHTML = countryList;
+})
+// Catch any errors
+.catch(error => console.error(error));
