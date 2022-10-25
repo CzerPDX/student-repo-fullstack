bd2f02408e000af5ee26512e092449eefb831ea5
CzerPDX <czer@pdx.edu>
2022-10-25 16:51:48 -0700

    removed countMultiples by simplifying. Also stopped using strings and instead worked with cents as integers

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -13,18 +13,6 @@ const DIME_VAL = 0.10;
 const NICKEL_VAL = 0.05;
 const PENNY_VAL = 0.01;
 
-// Count the multiples of the denomination and return the count
-const countMultiples = (currentTotal, denomination) => {
-    currentTotal = Number(currentTotal).toFixed(2) // Prevent rounding errors with toFixed to 2 decimal
-    let count = 0;
-    while (currentTotal >= denomination) {
-        currentTotal -= denomination;
-        currentTotal = currentTotal.toFixed(2);      // Prevent rounding errors with toFixed to 2 decimal
-        count += 1;
-    }
-    return count;
-}
-
 // Return the money name in English based on money value and amount
 const getMoneyName = (moneyCount, moneyValue) => {
     let moneyName = ``;
@@ -60,7 +48,7 @@ const getMoneyName = (moneyCount, moneyValue) => {
 }
 
 // Build the return string for each denomination
-const appendToString = (output, moneyCount, moneyValue, originalTotal) => {
+const formatOutputString = (output, moneyCount, moneyValue, originalTotal) => {
     if (moneyCount > 0) {
         // If output is empty, then we need to build the beginning of the string
         if (output == ``) {
@@ -88,62 +76,53 @@ const calculateChange = (input) => {
     if (input <= 10)
     {
         // Initialize denomination counts
-        let dollarCount = 0;                     // Number of dollars
-        let quarterCount = 0;                    // Number of quarters
-        let dimeCount = 0;                       // Number of dimes
-        let nickelCount = 0;                     // Number of nickels
-        let pennyCount = 0;                      // Number of pennies
-        let currentTotal = input.toString();     // Amount that will count down from input as denominations are removed
+        let currentCentsTotal = 0
 
-        // Find out how many dollars and subtract that amt from the currentTotal
-        if (currentTotal > 0) {
-            dollarCount = countMultiples(currentTotal, DOLLAR_VAL);
+        if (input >= 0) {
+            let dollarCount = Math.floor(input / DOLLAR_VAL);
             if (dollarCount > 0) {
-                currentTotal -= (dollarCount * DOLLAR_VAL);
-                returnString = appendToString(returnString, dollarCount, DOLLAR_VAL, input);
+                // Avoid rounding errors by working with currentCentsTotal as integers
+                currentCentsTotal = parseInt(input - (dollarCount * DOLLAR_VAL));
+                returnString = formatOutputString(returnString, dollarCount, DOLLAR_VAL, input);
             }
         }
-
+        else {
+            console.error(`Error! Must enter a positive number between 0 and 10!`)
+        }
+        
         // Find out how many quarters and subtract that amt from the currentTotal
         if (currentTotal > 0) {
-            quarterCount = countMultiples(currentTotal, QUARTER_VAL);
-            if (quarterCount > 0) {
-                currentTotal -= (quarterCount * QUARTER_VAL);
-                returnString = appendToString(returnString, quarterCount, QUARTER_VAL, input);
-            }
+            let quarterCount = Math.floor(currentCentsTotal / (QUARTER_VAL * 100));
+            currentCentsTotal -= quarterCount * (QUARTER_VAL * 100)
+            returnString = formatOutputString(returnString, quarterCount, QUARTER_VAL, input);
         }
 
         // Find out how many dimes and subtract that amt from the currentTotal
         if (currentTotal > 0) {
-            dimeCount = countMultiples(currentTotal, DIME_VAL);
-            if (dimeCount > 0) {
-                currentTotal -= (dimeCount * DIME_VAL);
-                returnString = appendToString(returnString, dimeCount, DIME_VAL, input);
-            }
+            let dimeCount = Math.floor(currentCentsTotal / (DIME_VAL * 100));
+            currentCentsTotal -= dimeCount * (DIME_VAL * 100)
+            returnString = formatOutputString(returnString, dimeCount, DIME_VAL, input);
         }
 
         // Find out how many nickels and subtract that amt from the currentTotal
         if (currentTotal > 0) {
-            nickelCount = countMultiples(currentTotal, NICKEL_VAL);
-            if (nickelCount > 0) {
-                currentTotal -= (nickelCount * NICKEL_VAL);
-                returnString = appendToString(returnString, nickelCount, NICKEL_VAL, input);
-            }
+            let nickelCount = Math.floor(currentCentsTotal / (NICKEL_VAL * 100));
+            currentCentsTotal -= nickelCount * (NICKEL_VAL * 100)
+            returnString = formatOutputString(returnString, nickelCount, NICKEL_VAL, input);
         }
 
         // Find out how many pennies and subtract that amt from the currentTotal
         if (currentTotal > 0) {
-            pennyCount = countMultiples(currentTotal, PENNY_VAL);
-            if (pennyCount > 0) {
-                currentTotal -= (pennyCount * PENNY_VAL);
-                returnString = appendToString(returnString, pennyCount, PENNY_VAL, input);
-            }
+            let pennyCount = Math.floor(currentCentsTotal / (PENNY_VAL * 100));
+            currentCentsTotal -= pennyCount * (PENNY_VAL * 100)
+            returnString = formatOutputString(returnString, pennyCount, PENNY_VAL, input);
         }
     }
     // If the input value is more than 10.00 return an error
     else {
         returnString = `\$${input} ==> Error: the number is too large`;
     }
+    
 
     return returnString;
 };
@@ -157,3 +136,8 @@ console.log(calculateChange(0.16));
 // $0.16 ==> 1 dime, 1 nickel, 1 penny
 console.log(calculateChange(15.11));
 // $15.11 ==> Error: the number is too large
+
+// // Test all possible values
+// for (let i = 0; i <= 1001; i++) {
+//     console.log(calculateChange(i * 0.01));
+// }
