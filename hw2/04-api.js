62817d77abbb4b82bacb7364941ccee24a0c6d79
CzerPDX <czer@pdx.edu>
2022-10-25 16:09:01 -0700

    04-api.js changes: added error display on page if error occurs loading the country list. Also reused 02-reverse.css and renamed to styles.css so it was more general use.

--- hw2/04-api.js
+++ hw2/04-api.js
@@ -17,16 +17,23 @@ fetch(url)
 // Get data
 .then((data) => {
 
-    let countryList = ``
+    let countryList = `<ol>`
     // Generate list items to insert into the DOM
     data.forEach(country => {
         // Save population formatted for the united states
         populationFormatted = country.population.toLocaleString("en-US");
         countryList += `<li> ${country.name.common} - ${populationFormatted}`;
     });
+    countryList += `</ol>`
 
     // Insert country list into page
     document.getElementById(`results`).innerHTML = countryList;
 })
 // Catch any errors
-.catch(error => console.error(error));
+.catch((error) => {
+    const resultsDiv = document.getElementById(`results`);
+    resultsDiv.className = `invalidInput`;
+    // Insert error into page and log to console.
+    resultsDiv.innerHTML = `Error! Unable to load Country List.`;
+    console.error(error);
+});
