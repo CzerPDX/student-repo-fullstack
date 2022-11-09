62d958561426bd0bb0bd74a24f5f8c59c23307bc
CzerPDX <czer@pdx.edu>
2022-11-09 15:44:50 -0800

    added solution for 03-form.js

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -19,8 +19,34 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
 // POST request
-app.post('', (req, res) => {
-  // Add your code here
+app.post('/submit', (req, res) => {
+  res.status(200);
+  res.set({ 'Content-Type': 'text/html' });
+
+  // Send out name and email info to page
+  // Should not be able to submit form without name and email so no check is needed here
+  res.write(`Name: ${req.body.inputName}<br>`);
+  res.write(`Email: ${req.body.inputEmail}<br>`);
+
+  // Process feedback input
+  if (req.body.inputFeedback === "") {
+    res.write(`Comments: n/a`);
+  }
+  else {
+    res.write(`Comments: ${req.body.inputFeedback}`);
+  }
+  res.write(`<br>`)
+
+  // Process Newsletter selection
+  if (req.body.inputNewsletter === undefined) {
+    res.write(`Newsletter: No, thank you.`)
+  }
+  else {
+    res.write(`Newsletter: Yes, sign me up for the newsletter.`);
+  }
+  res.write(`<br>`);
+
+  res.end();
 });
 
 app.listen(port, () => {
